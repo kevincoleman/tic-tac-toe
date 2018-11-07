68f7534b62f062346074870acd62f5f5c621b431
Cory Sutton <csutton68@icloud.com>
2018-11-06 21:03:47 -0800

    Add basics of a tic-tac-toe game

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Game from './components/game/Game';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      whoseMove: 'X',
+    };
+    this.switchMove = this.switchMove.bind(this);
+  }
+
+  switchMove() {
+    this.state.whoseMove === 'X'
+      ? this.setState({ whoseMove: 'O' })
+      : this.setState({ whoseMove: 'X' });
+  }
+
   render() {
     return (
       <div className="App">
-        <Game />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
+        <Game whoseMove={this.state.whoseMove} switchMove={this.switchMove} />
       </div>
     );
   }
--- packages/react-scripts/template/src/components/game/Game.jsx
+++ packages/react-scripts/template/src/components/game/Game.jsx
@@ -1,16 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
 class Game extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      spaceValue: '',
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick() {
+    this.playMove();
+  }
+
+  playMove() {
+    if (this.state.spaceValue === '') {
+      let played = this.props.whoseMove;
+      let whose = '';
+      this.props.whoseMove === 'X' ? (whose = 'O') : (whose = 'X');
+      this.props.switchMove();
+      this.setState({ spaceValue: played });
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>hello</h1>
-      </div>
-    )
+      <button onClick={this.handleButtonClick}>{this.state.spaceValue}</button>
+    );
   }
 }
 
